c3b9e9c1080376428676fc29e9dd5250f580de61
Damian <damian@danielcastelao.org>
2024-02-20 15:02:55 +0100

    Formulario de edición de datos

--- addons/openacademy/__manifest__.py
+++ addons/openacademy/__manifest__.py
@@ -16,14 +16,14 @@
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '1.1',
+    'version': '2.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'data/openacademy_data.xml',
